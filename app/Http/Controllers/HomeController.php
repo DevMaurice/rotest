b1bac54b782034fb72c2dce5a61dcd17be17da9b
DevMaurice <developermaurice@gmail.com>
2019-03-29 10:45:12 +0300

    get all leaves for a given user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $leaves = auth()->user()->leaves;
+
+        return view('home', \compact('leaves'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,55 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="flex justify-between items-center py-4 px-2 mb-4">
+    <div class="left">
+        Leaves
+    </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <div class="right">
+    <a class="py-2 px-2 bg-blue-darker text-white no-underline font-thin text-lg rounded" href="/leaves/create">
+        Apply Leave
+    </a>
+    </div>
+    </div>
 
-                    You are logged in!
-                </div>
+    <div class="flex justify-between items-center py-2 bg-grey-lighter mb-2 w-full">
+            <div>
+                Reason 
+            </div>
+
+            <div>
+                Date From
+            </div>
+
+            <div>
+                Date To
+            </div>
+
+            <div>
+                Days 
             </div>
         </div>
+    <div class="flex  justify-between w-full">
+        @foreach ($leaves as $leave)
+            <div class="flex justify-between items-center py-2 bg-grey-lighter w-full">
+                <div>
+                    {{ $leave->reason }}
+                </div>
+
+                <div>
+                    {{ $leave->date_from }}
+                </div>
+
+                <div>
+                    {{ $leave->date_to }}
+                </div>
+
+                <div>
+                    {{ $leave->days }} days
+                </div>
+            </div>
+        @endforeach
     </div>
 </div>
 @endsection
